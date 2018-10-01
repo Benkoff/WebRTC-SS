686792a744e5ac5beaf7d1d8294d0bb4d72b444e
benkoff <mailto.benkoff@gmail.com>
2018-10-01 03:07:53 +0300

    Add Room Template, Service, Controller Room Logic

--- src/main/java/io/github/benkoff/webrtcss/controller/MainController.java
+++ src/main/java/io/github/benkoff/webrtcss/controller/MainController.java
@@ -1,11 +1,13 @@
 package io.github.benkoff.webrtcss.controller;
 
 import io.github.benkoff.webrtcss.domain.Room;
+import io.github.benkoff.webrtcss.domain.RoomService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,48 +21,75 @@ import java.util.TreeSet;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Controller
+@ControllerAdvice
 public class MainController {
-    // domain service layer substitution since this is a very simple realization w/o services & repos
-    private final Set<Room> rooms = new TreeSet<>(Comparator.comparing(Room::getId));
+    private final RoomService roomService;
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    public MainController(RoomService roomService) {
+        this.roomService = roomService;
+    }
+
     @GetMapping({"", "/", "/index", "/home", "/main"})
-    public ModelAndView displayMainPage(Long id) {
+    public ModelAndView displayMainPage(Long id, String uuid) {
         ModelAndView modelAndView = new ModelAndView("main");
         modelAndView.addObject("id", id);
-        modelAndView.addObject("rooms", rooms);
+        modelAndView.addObject("rooms", roomService.getRooms());
+        modelAndView.addObject("uuid", uuid);
 
         return modelAndView;
     }
 
-    @PostMapping("/room")
-    public ModelAndView processRoomSelection(@ModelAttribute("id") String sid, BindingResult bindingResult, Model model) {
+    @PostMapping(value = "/room", params = "action=create")
+    public ModelAndView processRoomSelection(@ModelAttribute("id") String sid,
+                                             @ModelAttribute("uuid") String uuid,
+                                             BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             // simplified release, no errors processing
             return new ModelAndView("redirect:/");
         }
-        parseId(sid).ifPresent(number -> rooms.add(new Room(number)));
-        logger.debug("Create Room#{}", parseId(sid).map(Object::toString).orElse("Error parsing Id!"));
+        parseId(sid).ifPresent(number -> {
+            Optional.ofNullable(uuid).ifPresent(name -> {
+                Room room = new Room(number);
+                roomService.getRooms().add(room);
+                room.setHostName(name);
+                logger.debug("Host {} creates Room#{}", name, number);
+            });
+        });
 
         return new ModelAndView("redirect:/");
     }
 
-    @GetMapping("/room/{sid}")
-    public ModelAndView displaySelectedRoom(@PathVariable("sid") String sid) {
-        logger.debug("Select Room#{}", parseId(sid).map(Object::toString).orElse("Error parsing Id!"));
+    @GetMapping("/room/{sid}/user/{uuid}")
+    public ModelAndView displaySelectedRoom(@PathVariable("sid") String sid,
+                                            @PathVariable("uuid") String uuid) {
+        // Link to redirect if provided data invalid
         ModelAndView modelAndView = new ModelAndView("redirect:/");
-        parseId(sid).ifPresent(id -> {
-            rooms.stream().filter(r -> r.getId().equals(id)).findFirst().ifPresent(room -> {
-                //TODO create Room model
-            });
-        });
+
+        if (parseId(sid).isPresent()) {
+            // find the room
+            Room room = roomService.getRooms().stream()
+                    .filter(r -> r.getId().equals(parseId(sid).get()))
+                    .findFirst()
+                    .orElse(null);
+            if(room != null) {
+                // add this visitor if not a host
+                if (!room.getHostName().equals(uuid)) {
+                    room.setVisitorName(uuid);
+                }
+                logger.debug("User {} joins Room#{}", uuid, sid);
+                // send to the room
+                modelAndView = new ModelAndView("chat_room", "id", sid);
+                modelAndView.addObject("uuid", uuid);
+            }
+        }
 
         return modelAndView;
     }
 
     @GetMapping("/room/random")
-    public ModelAndView requestRandomRoomNumber() {
-        return displayMainPage(randomValue());
+    public ModelAndView requestRandomRoomNumber(@ModelAttribute("uuid") String uuid) {
+        return displayMainPage(randomValue(), uuid);
     }
 
     @GetMapping("/offer")
---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,24 @@
+$(function(){
+    const uuidInput = document.querySelector('input#uuid');
+
+    function guid() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
+
+    if (localStorage.getItem("uuid") === null) {
+        localStorage.setItem("uuid", guid());
+    }
+    uuidInput.value = localStorage.getItem("uuid");
+    console.log("local.uuid:" + localStorage.getItem("uuid"));
+    console.log("input.value:" + uuidInput.value);
+});
+
+function addUuidToButtonLink(button) {
+    let id = 'button-link-' + button.value;
+    let ref = document.getElementById(id).href;
+    document.getElementById(id).href = ref + '/user/' + localStorage.getItem("uuid");
+    console.log("link.href:" + document.getElementById(id).href);
+}
